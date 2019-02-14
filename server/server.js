ac9449ae9f0cd820030117f791ffb5a04124a95a
Muhammet ESER <muhammeteser29@gmail.com>
2019-02-14 17:59:09 +0300

    added m:m to backend

--- server/server.js
+++ server/server.js
@@ -1,52 +1,86 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const { getEntityDataType } = require('./utils/');
-const { sequelize } = require('./config/sequelize')
+const { getEntityDataType } = require("./utils/");
+const { sequelize } = require("./config/sequelize");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/tables', (req, res) => {
+app.post("/tables", (req, res) => {
   const tables = req.body;
-
   tables.sort((a, b) => a.multi - b.multi);
 
+  // ********** //
+  //Create Tables
   tables.forEach(table => {
-
-    if (table.multi == 2) {
-      table.entityItems = "";
-    }
-
-    let columns = {}
+    let columns = {};
 
     for (let i in table.entityItems) {
-      columns[table.entityItems[i].itemName] = { type: getEntityDataType(table.entityItems[i].dataType), allowNull: false };
+      /* if (table.multi == 2) {
+        // Set PK
+        columns[table.entityItems[0].itemName] = {
+          type: getEntityDataType(table.entityItems[0].dataType),
+          primaryKey: true,
+          allowNull: false
+        };
+        // Set PK
+        columns[table.entityItems[1].itemName] = {
+          type: getEntityDataType(table.entityItems[1].dataType),
+          primaryKey: true,
+          allowNull: false
+        };
+      } */
+
+      columns[table.entityItems[i].itemName] = {
+        type: getEntityDataType(table.entityItems[i].dataType),
+        allowNull: false
+      };
+
+      // Set PK
+      columns[table.entityItems[0].itemName] = {
+        type: getEntityDataType(table.entityItems[0].dataType),
+        primaryKey: true,
+        allowNull: false
+      };
     }
-    table.entityName = sequelize.define(table.entityName, columns, { timestamps: false });
+
+    table.entityName = sequelize.define(table.entityName, columns, {
+      underscored: true,
+      timestamps: false
+    });
   });
 
   // Creating M:M realtion table
   for (let i = 0; i < tables.length - 1; i++) {
     // Zorunlu taraf
     if (tables[i].entityType == "mandatory") {
-      tables[tables.length - 1].entityName.belongsTo(tables[i].entityName, { foreignKey: { allowNull: false } });
+      tables[tables.length - 1].entityName.belongsTo(tables[i].entityName, {
+        foreignKey: { allowNull: false }
+      });
     } else if (tables[i].entityType == "optional") {
-    // Seçimli taraf
-      tables[tables.length - 1].entityName.belongsTo(tables[i].entityName, { foreignKey: { allowNull: true } });
+      // Seçimli taraf
+      tables[tables.length - 1].entityName.belongsTo(tables[i].entityName, {
+        foreignKey: { allowNull: true }
+      });
     }
   }
 
   tables.forEach(table => {
-    table.entityName.sync({ force: true })
-  })
+    if (table.entityName) {
+      table.entityName.sync({ force: true });
+    } else {
+      setTimeout(() => {
+        tables[2].entityName.sync({ force: true });
+      }, 1000);
+    }
+  });
 
-  res.sendStatus(200)
+  res.sendStatus(200);
 });
 
-
 app.listen(3000, () => {
-  console.log('Started on port ', 3000);
+  console.log("Started on port ", 3000);
 });
