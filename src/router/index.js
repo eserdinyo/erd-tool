16e4a126e8a1334060e7b3df599d47daf7e03296
Muhammet ESER <muhammeteser29@gmail.com>
2019-02-24 15:43:52 +0300

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
-import Main from '../views/Main'
-import Tables from '../views/Tables'
+
+
+import Main from "../views/Main";
+import Tables from "../views/Tables";
 export default new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Main,
-        },
-        {
-            path: '/tables',
-            name: 'tables',
-            component: Tables,
-        }
-    ]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Main
+    },
+    {
+      path: "/tables",
+      name: "tables",
+      component: Tables
+    }
+  ]
+});
 
 
